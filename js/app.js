22dd596743ea4292bfd90c879a69b77540184879
emtf504 <tx504sboy@icloud.com>
2025-08-07 06:22:11 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+  const audio = new Audio('assets/day0_reflection.mp3');
+  audio.play().catch((e) => {
+    console.log('Autoplay failed:', e);
+  });
+});
